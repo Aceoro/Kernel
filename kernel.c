f8845f164714da1304f911c2170a8b3a7db6ec36
ace <34866740+wholivesinapineappleunderthesea@users.noreply.github.com>
2018-02-06 18:05:52 -0500

    Add files via upload

--- kernel.c
+++ kernel.c
@@ -1,28 +1,15 @@
 /*
 *  kernel.c
-*  hopefully one day will be my crypto mining kernel
+*  probably won't ever be anything
 */
-
+#include "kernel.h"
 
 unsigned int written = 0;
-void print(const char *prnt) {
-	// video memory starts at 0xb8000, +140 for new line
-	// TODO: find some better way that doesn't use if statements
-	// that is more programatic
-	char *vidptr;
-	if (written == 0) {
-		vidptr = (char*)0xb8000;
-	} else if (written == 1) {
-		vidptr = (char*)0xb8140;
-	} else if (written == 2) {
-		vidptr = (char*) 0xb8280;
-	} else {
-		clear();
-	}
+void print(const char *prnt, char *vidptr) {
 	unsigned int i = 0;
 	unsigned int j = 0;
 
-	while(prnt[j] != '\0') {
+	while (prnt[j] != '\0') {
                 vidptr[i] = prnt[j];
                 vidptr[i+1] = 0x07;
                 ++j;
@@ -32,16 +19,13 @@ void print(const char *prnt) {
 	return;
 }
 
+
 void clear(void) {
-	// used as an easy way to detect error within the print()
-	// function, if used elsewhere, it isn't an error
-	// but rather a way to clear the screen from all the garbage
-	// I write
 	char *vidptr = (char*)0xb8000;
 	unsigned int j = 0;
-	while(j < 80 * 25 * 2) {
+	while (j < 80 * 25 * 2) {
 		vidptr[j] = ' ';
-		vidptr[j+1] = 0x07; // 0x07 is light grey, 
+		vidptr[j+1] = 0x07;
 		j = j + 2;
 	}
 
@@ -51,10 +35,26 @@ void clear(void) {
 	return;
 }
 
-void kmain(void) {
-	clear(); // clear() used here to remove all qemu strings
-	print("string 1"); // I use these to test, they aren't special
-	print("string 2");
-	print("string 3");
+void disperr(const char *error) {
+	clear();
+	char *vidptr = (char*)0xb8000;
+	unsigned int i = 0;
+	unsigned int j =  0;
+        while (error[j] != '\0') {
+                vidptr[i] = error[j];
+		vidptr[i+1] = 0x04;
+		++j;
+                i = i + 2;
+        }
+
 	return;
 }
+
+
+void kmain(void) {
+	clear(); // clear() used here to remove all qemu strings
+	print("ur mom big gay", (char*)0xb8000);
+	//disperr("an error has occured"); // error test
+	return;
+}
+
---
+++ kernel.h
@@ -0,0 +1,4 @@
+void kmain(void);
+void print(const char *prnt, char *vidptr);
+void clear(void);
+void disperr(const char *error);
